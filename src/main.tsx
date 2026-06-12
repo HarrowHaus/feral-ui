848651da8de8173f755dbe10c7fd41993f20ae0e
HarrowHaus <donald@harrow.haus>
2026-06-12 01:30:32 -0500

    ornaments: load ornament route styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/feral.css";
 import "./styles/template-previews.css";
+import "./styles/ornaments-page.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
