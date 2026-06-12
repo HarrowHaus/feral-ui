1da37e5f40d6a29b7b1bd7edb390e9d046916538
HarrowHaus <donald@harrow.haus>
2026-06-12 02:23:01 -0500

    closed-loop: load self label styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./styles/feral.css";
 import "./styles/template-previews.css";
 import "./styles/ornaments-page.css";
 import "./styles/component-playground.css";
+import "./styles/self-labels.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
